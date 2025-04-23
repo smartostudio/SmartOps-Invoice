7bc23b5e985a584e0219d1c34f9aa238d83154fb
smartostudio <smartostudio@gmail.com>
2025-04-23 23:23:35 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,21 @@
-// src/firebaseConfig.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyCNNA-qrWBGaI3ohI5o2LFLAOLztiAzqYI",
+  authDomain: "smartopsinvoice.firebaseapp.com",
+  projectId: "smartopsinvoice",
+  storageBucket: "smartopsinvoice.firebasestorage.app",
+  messagingSenderId: "667698373662",
+  appId: "1:667698373662:web:cc1fae6e34ebf0d5f03a0f",
+  measurementId: "G-RQFB04B41N"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
